2c0cc4871446224782da5e9c2c77a28c74323d4f
Kirill <test@mail.ru>
2021-12-11 13:45:54 +0300

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module ru.kirill.chess.chess {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ru.kirill.chess to javafx.fxml;
+    exports ru.kirill.chess;
+}
